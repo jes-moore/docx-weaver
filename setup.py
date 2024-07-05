eefcceb6730a0b4c3e019440a0b8c5bc0a8be4a4
Jesse Moore <jessemoore07@gmail.com>
2024-07-05 16:56:05 -0600

    update name for pypi, extend .gitignore

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 with open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 setup(
-    name="wordweaver",
+    name="word-weaver-docx",
     version="0.1.0",
     url="https://github.com/jes-moore/word-weaver",
     author="Jesse Moore",
